e948be100b0068a87f149078594c08747fc42391
evanbern59 <evansantos1202@gmail.com>
2025-11-30 13:43:42 -0800

    answered questions

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("staying consistant with the readings and lectures gave me a good learning process for the course.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("I would change it to be a full term class instead of half a semester. I dont really like that everything is fast paced");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I defently learned a lot I just hope i find some use with it");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("Everything was new to me as I had never taken a js class before just CSS and HTML");
